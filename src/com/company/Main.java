868dfe4be8af7522e6717dec948b0d950830a4c9
iaroslav.sviatkin <iaroslav.sviatkin.globallogic.com>
2020-11-22 14:19:02 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.stream.IntStream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int sum=0;
+        for (String s : "3241".split("")) {
+            System.out.println(Integer.parseInt(s));
+        }
+
+
+    }
+}
+
